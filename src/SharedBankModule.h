b87c451a869d45639fe8204a57ae4065490ce871
AnnuitNovus <cdk675@gmail.com>
2026-01-01 12:58:14 -0700

    Refactor SharedBankModule header with new structure

--- src/SharedBankModule.h
+++ src/SharedBankModule.h
@@ -1,9 +1,29 @@
-class SharedBankModule : public Module {
-public:
-    SharedBankModule();
-    // Override hooks here, e.g.:
-    bool OnCastSpell(Player* player, Spell* spell) override;
-    void OnItemMove(Player* player, Item* item, uint8 srcBag, uint8 srcSlot, uint8 dstBag, uint8 dstSlot) override;
-};
-// At end:
-static SharedBankModule sharedBankModule;
+#ifndef SHAREDBANKMODULE_H
+#define SHAREDBANKMODULE_H
+
+#include "Entities/Item.h"
+#include "Entities/Player.h"
+#include "Module.h"
+#include "SharedBankModuleConfig.h"
+#include "Spells/Spell.h"
+#include <cstdint>
+
+namespace cmangos_module
+{
+
+    class SharedBankModule : public Module
+    {
+    public:
+        SharedBankModule();
+        const SharedBankModuleConfig* GetConfig() const;
+
+        bool OnCastSpell(Player* player, Spell* spell);
+        void OnItemMove(Player* player, Item* item, uint8 srcBag, uint8 srcSlot, uint8 dstBag, uint8 dstSlot);
+    private:
+        void OpenSharedBank(Player* player);
+    };
+
+    extern SharedBankModule sharedBankModule;
+
+} // namespace cmangos_module
+#endif
