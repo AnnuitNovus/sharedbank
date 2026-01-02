1cbed6e9569dd52b19bf9b33475f92574338a3a3
AnnuitNovus <cdk675@gmail.com>
2026-01-01 23:37:01 -0700

    Update characters.sql

--- sql/uninstall/characters/characters.sql
+++ sql/uninstall/characters/characters.sql
@@ -1,6 +1,1 @@
-CREATE TABLE IF NOT EXISTS account_bank (
-  account_id INT UNSIGNED NOT NULL,
-  slot TINYINT UNSIGNED NOT NULL,
-  item_guid INT UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (account_id, slot)
-);
+DROP TABLE IF EXISTS account_bank;
