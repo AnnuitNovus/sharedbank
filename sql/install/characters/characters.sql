3eaa47fd4c51ef10ce3cf454d1ac1ef3c4fba3d6
AnnuitNovus <cdk675@gmail.com>
2026-01-01 23:36:19 -0700

    Add foreign key constraint to account_bank table

--- sql/install/characters/characters.sql
+++ sql/install/characters/characters.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS account_bank (
   account_id INT UNSIGNED NOT NULL,
   slot TINYINT UNSIGNED NOT NULL,
   item_guid INT UNSIGNED DEFAULT NULL,
-  PRIMARY KEY (account_id, slot)
-);
+  PRIMARY KEY (account_id, slot),
+  KEY item_guid (item_guid),
+  CONSTRAINT fk_account_bank_item FOREIGN KEY (item_guid) REFERENCES item_instance (guid) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Account Shared Bank';
