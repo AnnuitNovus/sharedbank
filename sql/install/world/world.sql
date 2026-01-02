dbc329573aafa8409c1eb3e1eee819cc509edbcd
AnnuitNovus <cdk675@gmail.com>
2026-01-02 00:16:05 -0700

    Modify spell_template insert statement

--- sql/install/world/world.sql
+++ sql/install/world/world.sql
@@ -1,2 +1,2 @@
-INSERT INTO spell_template (Id, Attributes, AttributesEx, Effect0, EffectImplicitTargetA0) VALUES 
-(90000, 0x00000008, 0x00000000, 3, 1); 
+INSERT INTO spell_template (Id, Attributes, AttributesEx, Effect1, EffectImplicitTargetA1, SpellName) VALUES 
+(90000, 0x00000008, 0x00000000, 3, 1, 'Shared Bank');
